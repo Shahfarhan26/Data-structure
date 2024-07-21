64e9de8062ee72fcd8e0e51f42e938a7cb32b123
Shahfarhan26 <135253843+Shahfarhan26@users.noreply.github.com>
2024-07-21 13:02:55 +0530

    Add files via upload

---
+++ greatestcommondivisor.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int gcd(int a,int b);
+int main(){
+    int a,b;
+    printf("enter two  positive integers :\n");
+    scanf("%d %d",&a,&b);
+    printf("the greatest common integer between %d and %d is %d \n:",a,b,gcd(a,b));
+    return 0;
+}
+int gcd(int a,int b){
+    if (b == 0)
+    {
+        return a;
+    }else{
+        return gcd(b , a % b);
+    }
+}
